50292e94a8fee587caa8ece63c547c5b5f644a53
Eugene <antipov.jn@gmail.com>
2019-06-23 19:03:50 +0300

    HW 14.0

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.Map.Entry;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Arrays.stream(new Scanner(System.in).nextLine().toLowerCase().split("[\\p{Punct}\\s]+"))
+                .collect(Collectors.toMap(a -> a, a -> 1, (a, b) -> a + b)).entrySet()
+                .stream().sorted(new EntryComparator()).limit(10).forEach(x -> System.out.println(x.getKey()));
+    }
+
+    public static class EntryComparator implements Comparator<Entry<String, Integer>> {
+        @Override
+        public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+            if (o1.getValue() > o2.getValue()) {
+                return -1;
+            } else if (o1.getValue() < o2.getValue()) {
+                return 1;
+            } else {
+                return o1.getKey().compareTo(o2.getKey());
+            }
+        }
+    }
+}
